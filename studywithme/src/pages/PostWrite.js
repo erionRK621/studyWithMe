e54addd861035593af0817fe4ad09f34787dc40d
kimhanjune93 <kimhanjune93@gmail.com>
2021-10-28 12:29:42 +0900

    feat: ckeditor 추가

--- studywithme/src/pages/PostWrite.js
+++ studywithme/src/pages/PostWrite.js
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Editor from "../components/Editor";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2,
+} from "react-html-parser";
 const PostWrite = () => {
-  return <div></div>;
+  const [post, setPost] = useState("");
+  const posting = () => {};
+  const getContent = (content) => {
+    setPost(ReactHtmlParser(content));
+  };
+  return (
+    <div>
+      <div style={{height:"100px"}}></div>
+      <Editor getContent={getContent} />
+      <button onClick={posting}>작성</button>
+      {post}
+    </div>
+  );
 };
 
 export default PostWrite;
