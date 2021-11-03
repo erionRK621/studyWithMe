20feef6a8b77cdb61f09e67112a10a1c7b37d6e9
erionRK621 <jhkim62134@naver.com>
2021-11-03 12:18:40 +0900

    Update Mypage.js

--- studywithme/src/pages/Mypage.js
+++ studywithme/src/pages/Mypage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { AiTwotoneSetting } from "react-icons/ai";
+
 import Image from "../elements/Image";
 import Text from "../elements/Text";
 
@@ -8,19 +10,23 @@ const Mypage = () => {
     <React.Fragment>
       <UserInfo>
         <LeftDiv>
-          <Image>프로필 사진</Image>
+          <ProfileImg>
+            <Image size="100"></Image>
+          </ProfileImg>
         </LeftDiv>
 
         <RightDiv>
-          <UpDiv>
-            <Text>닉네임</Text>
-            <EditBtn>수정버튼</EditBtn>
-          </UpDiv>
-          <DownDiv>
+          <TopDiv>
+            <AiTwotoneSetting cursor="pointer" size="1.7em"></AiTwotoneSetting>
+          </TopDiv>
+          <MiddleDiv>
+            <Nickname>닉네임</Nickname>
+          </MiddleDiv>
+          <BottomDiv>
             <Text>게시글 1개</Text>
             <Text>팔로우 2명</Text>
             <Text>팔로잉 3명</Text>
-          </DownDiv>
+          </BottomDiv>
         </RightDiv>
       </UserInfo>
       <div>포스트들</div>
@@ -38,25 +44,39 @@ const UserInfo = styled.div`
 const LeftDiv = styled.div`
   width: 30%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
+const ProfileImg = styled.div`
+  height: 77px;
+  width: 77px;
+`;
+
 const RightDiv = styled.div`
   width: 70%;
   height: 100%;
 `;
-const UpDiv = styled.div`
+const TopDiv = styled.div`
   display: flex;
-  justify-content: space-between;
-  height: 50%;
-  padding: 40px;
-  padding-right: 0;
+  height: 10%;
+  flex-direction: row-reverse;
 `;
-const DownDiv = styled.div`
+const MiddleDiv = styled.div`
+  display: flex;
+  height: 40%;
+  padding: 40px 40px 40px 60px;
+`;
+const Nickname = styled.div`
+  font-size: 48px;
+`;
+
+const BottomDiv = styled.div`
   display: flex;
   justify-content: space-evenly;
   width: 100%;
   height: 50%;
   margin: auto;
 `;
-const EditBtn = styled.button``;
 
 export default Mypage;
