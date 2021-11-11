321899b3df632a1dcec50f2477141fdc7db28d4f
ynjnkn <yjun_k@hotmail.com>
2021-11-11 12:26:57 +0900

    팔로워, 팔로우 모달 완성

--- studywithme/src/components/FollowModal.js
+++ studywithme/src/components/FollowModal.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import dotenv from "dotenv";
 
-// Modal.jsx
+// Font Awesome-related
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes as fasTimes } from '@fortawesome/free-solid-svg-icons'
 
-const FollowModal = ({ modalClose }) => {
+// Components
+import Image from "../elements/Image";
+
+const FollowModal = ({ modalClose, followingList }) => {
+  const dispatch = useDispatch();
+
+  console.log("followingList", followingList);
+
   const onCloseModal = (e) => {
-    console.log("e.target: ", e.target);
-    console.log("e.tarcurrentTargetget: ", e.currentTarget);
+    // console.log("e.target: ", e.target);
+    // console.log("e.tarcurrentTarget", e.currentTarget);
     if (e.target === e.currentTarget) {
       modalClose();
     }
@@ -15,10 +26,40 @@ const FollowModal = ({ modalClose }) => {
     <Container onClick={onCloseModal}>
       <Modal>
         <ModalTitle>
-          <div>팔로우 목록</div>
-          {/* <ModalBtn onClick={modalClose}> X</ModalBtn> */}
+          <Title>팔로우</Title>
+          <CloseButton>
+            <FontAwesomeIcon
+              onClick={onCloseModal}
+              style={{
+                width: "24px",
+                height: "24px",
+                color: "#9E9D9D"
+              }}
+              icon={fasTimes} />
+          </CloseButton>
         </ModalTitle>
-        <div>팔로우 사람들 맵돌리기</div>
+        <ModalBody>
+          {followingList.map((following, idx) => {
+            return (
+              <FlexGrid
+                key={following.userId}
+              >
+                <UserWrapper>
+                  <Image
+                    size="56"
+                    src={`${process.env.REACT_APP_API_URI}/${following.avatarUrl}`}
+                  />
+                  <UserName
+                    size="15px"
+                  >
+                    {following.nickname}
+                  </UserName>
+                </UserWrapper>
+                <UnfollowButton>언팔로우</UnfollowButton>
+              </FlexGrid>
+            )
+          })}
+        </ModalBody>
       </Modal>
     </Container>
   );
@@ -33,30 +74,73 @@ const Container = styled.div`
   top: 0;
   left: 0;
 `;
+
 const Modal = styled.div`
-  width: 400px;
-  height: 600px;
+  width: 565px;
+  height: 613px;
   background-color: #fff;
-  // Modal 창 브라우저 가운데로 조정
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   z-index: 100;
+  border-radius: 20px;
 `;
+
+const CloseButton = styled.div`
+position: absolute;
+top: 26.5px;
+right: 56px;
+border: none;
+background-color: white;
+cursor: pointer;
+`
 
 const ModalTitle = styled.div`
   display: flex;
   justify-content: center;
-  border-bottom: solid 1px black;
-  padding: 10px;
+  border-bottom: solid 1px #d3d3d3;
+  padding: 25px;
+  margin-bottom: 26px;
 `;
 
-// const ModalBtn = styled.button`
-//   position: relative;
-//   left: 30%;
-//   top: 60%;
-//   transform: translate(-50%, -50%);
-// `;
+const Title = styled.div`
+font-style: normal;
+font-weight: normal;
+font-size: 20px;
+line-height: 28px;
+color: #000000;
+`;
+
+const ModalBody = styled.div`
+`;
+
+const FlexGrid = styled.div`
+display: flex;
+justify-content: space-between;
+margin-left: 5px;
+margin: 0px 46px 16px 46px;
+align-items: center;
+`;
+
+const UserWrapper = styled.div`
+display: flex;
+align-items: center;
+`;
+
+const UserName = styled.p`
+margin-left: 26px;
+`;
+
+const UnfollowButton = styled.button`
+width: 70px;
+height: 27px;
+color: white;
+background: #369C8A;
+border: none;
+border-radius: 4px;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+cursor: pointer;
+`;
 
 export default FollowModal;
